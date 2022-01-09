157f4f06019e732c81f5889c114ba49f171f156a
mrvolkov2 <mrvolkov2@yandex.ru>
2022-01-09 15:16:46 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let nToString = Math.abs(n).toString();
+    let arr = nToString.split('');
+    let reverseArr = arr.reverse();
+    let reverseStr = reverseArr.join('');
+    let numberReverse = Number(reverseStr);
+    return numberReverse;
 }
